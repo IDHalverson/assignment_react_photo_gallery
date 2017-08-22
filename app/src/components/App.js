35c90ca52d5a5ab75e07fa62019147e539fde717
Ian Halverson <halversoncode@gmail.com>
2017-08-22 13:11:26 -0400

    got one sorting thing done

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import logo from "../logo.svg";
 import "../App.css";
 import { PhotosDisplay } from "./PhotosDisplay";
+import { Button } from "./Button";
 
 const photos = require("../photos").data.map(post => {
   return {
@@ -28,19 +29,50 @@ class App extends Component {
         photos[3],
         photos[4],
         photos[5]
-      ]
+      ],
+      timeFilter: "DESC"
     };
   }
-  TimeFilter(e) {}
+
+  handleTimeFilter = () => {
+    const order = this.state.timeFilter;
+    photos.sort(function(a, b) {
+      if (a.createdTime > b.createdTime) {
+        if (order === "DESC") {
+          return -1;
+        } else {
+          return 1;
+        }
+      } else {
+        if (order === "DESC") {
+          return 1;
+        } else {
+          return -1;
+        }
+      }
+    });
+    this.setState({
+      displayedPhotos: [
+        photos[0],
+        photos[1],
+        photos[2],
+        photos[3],
+        photos[4],
+        photos[5]
+      ],
+      timeFilter: order === "DESC" ? "ASC" : "DESC"
+    });
+  };
+
   render() {
-    let { displayedPhotos } = this.state;
+    let { displayedPhotos, handleTimeFilter, timeFilter } = this.state;
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <button onClick={TimeFilter}> Descending </button>
+        <Button handler={this.handleTimeFilter} sortBy={timeFilter} />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
--- app/src/components/PhotosDisplay.js
+++ app/src/components/PhotosDisplay.js
@@ -1,22 +1,19 @@
 import React, { Component } from "react";
-const photos = require("../photos").data.map(post => {
-  return {
-    image: post.images.standard_resolution.url,
-    user: post.user.username,
-    caption: post.caption ? post.caption.text : "",
-    likes: post.likes ? post.likes.count : "",
-    createdTime: Number(post.created_time),
-    filter: post.filter,
-    numberOfComments: post.comments.length
-  };
-});
+
 class PhotosDisplay extends Component {
   render() {
     return (
       <div className="container">
         {this.props.currentPhotos.map(el => {
           console.log(el.user);
-          return <img src={el.image} key={el.image} alt="logo" />;
+          return (
+            <div>
+              <img src={el.image} key={el.image} alt="logo" />
+              <h6>
+                {el.createdTime}
+              </h6>
+            </div>
+          );
         })}
       </div>
     );
